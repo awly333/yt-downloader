e68675ac82fb94527b0985a614819b3d83fc6a0c
awly333 <189546988+awly333@users.noreply.github.com>
2026-03-27 21:02:28 -0700

    fix: correct renderer index.html path in packaged app (was ../dist, needs ../../dist)

--- src/main/index.ts
+++ src/main/index.ts
@@ -80,7 +80,7 @@ function createWindow() {
   if (isDev) {
     mainWindow.loadURL('http://localhost:5173')
   } else {
-    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'))
+    mainWindow.loadFile(path.join(__dirname, '../../dist/index.html'))
   }
 
   mainWindow.on('closed', () => {
